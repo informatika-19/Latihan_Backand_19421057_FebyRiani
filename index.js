398e4d1d01d6eaf1c45092538c220b6c3f537d6b
FebyRiani <febyriani63@gmail.com>
2021-04-19 04:26:13 +0100

    latihan-routes

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
- const { console } = require('console')
+const { Console } = require('console')
 const express = require('express')
 const app = express()
 const bodyParser= require('body-parser')
+
+// export dari satu folder
+
+
 
 app.use(bodyParser.json({
     extend: true,
     limit: '20mb'
 }))
-
 app.use(bodyParser.urlencoded({
-    extend:true,
-    limit:'20mb'
+    extend: true,
+    limit: '20mb'
 }))
-
 app.get('/', (req, res)=>{
     res.send('Hello World 1!')
-    
 })
-
 app.get('/profile/:username/:id', (req, res)=>{
     console.log(req.params)
     res.send('username = '+ req.params.username)
 })
-
-app.get('/daerah/:daerah', (req, res)=>{
+// reg params
+app.get('/daerah/:namadaerah', (req, res)=>{
     console.log(req.params)
-    res.send('daerah anda = '+ req.params.daerah)
+    res.send('daerah anda = '+ req.params.namadaerah)
 })
-
-app.post('/register', (reg, res)=>{
+// reg body
+app.post('/register', (req, res)=> {
     console.log(req.body)
-    res.json(req.body)
+    // res.json(req.body)
 })
-
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('server started')
 })
